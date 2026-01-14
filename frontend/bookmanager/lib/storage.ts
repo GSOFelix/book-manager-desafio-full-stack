91ac50cefda7e5f74f6022a76b672477259c6bfa
Guilherme Felix <guilhermedesoares@gmail.com>
2026-01-13 22:49:44 -0300

    feat: criado funçoes de localstorage

---
+++ frontend/bookmanager/lib/storage.ts
@@ -0,0 +1,27 @@
+const TOKEN_KEY = 'bm_token';
+const USER_KEY = 'bm_user';
+
+export function setSession(token:string,user:string){
+    localStorage.setItem(TOKEN_KEY,token);
+    localStorage.setItem(USER_KEY,JSON.stringify(user))
+}
+
+export function getToken(){
+    if(typeof window === 'undefined') return null;
+    return localStorage.getItem(TOKEN_KEY);
+}
+
+export function getUser(){
+    if(typeof window === 'undefined') return null;
+    const rawUser = localStorage.getItem(USER_KEY);
+    return rawUser ? JSON.parse(rawUser): null;
+}
+
+export function clearSession(){
+    localStorage.removeItem(TOKEN_KEY);
+    localStorage.removeItem(USER_KEY);
+}
+
+export function isAuthenticated(){
+    return !!getToken()
+}
