7c4375f9e63b7b02fb67f16bf66ca3e6529842f8
GSOFelix <guilhermedesoares@gmail.com>
2026-01-14 14:48:10 -0300

    refactor: removido importações desnecessarias

--- frontend/bookmanager/app/layout.tsx
+++ frontend/bookmanager/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "BookManager",
@@ -25,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body
-        className="min-h-screen bg-gray-50 text-slate-800"
+        className="min-h-screen bg-black text-green-100"
       >
         {children}
       </body>
